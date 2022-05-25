39ef500330843cc8d49ca587ba128776aeb066f0
adinamfranco <adinamfranco@gmail.com>
2022-05-25 08:04:21 -0300

    add gitignore file

--- apis/bacen_exchange.py
+++ apis/bacen_exchange.py
@@ -1,10 +1,11 @@
 import requests
 
-url = r"\"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?%40moeda='USD'&%40dataInicial='05-19-2022'&%40dataFinalCotacao='05-20-2022'&%24format=json\" -H  \"accept: application/json;odata.metadata=minimal\""
+url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='05-24-2022'&$format=json"
 
 payload={}
 headers = {
-  'Cookie': 'TS01d9825e=0198c2d644685419e8d718bda30bd7fb8ced1bb2ab10bd25f985d165fd4bf845b798f748e95cb371fdd51b29f9024630217f87bfe4; dtCookie=BD0F6C6B9627F42D46BCB6B832C83755|cHRheHwx; JSESSIONID=0000KtB8Ck892N6lC5vp-Wrwdqc:1dof85j5r; TS013694c2=0198c2d644685419e8d718bda30bd7fb8ced1bb2ab10bd25f985d165fd4bf845b798f748e95cb371fdd51b29f9024630217f87bfe4'
+  'accept': 'application/json;odata.metadata=minimal',
+  'Cookie': 'TS01d9825e=0198c2d64430855160404b690b35fb7fe1bb31317d1763e8ea9dadeafcc75e5e4eb8e46e80da91e38394ceeb1750446fc8da49f95c; dtCookie=78505100B138008A1CA7E84F550FE09A|cHRheHwx; BIGipServer~was-p_as3~was-p~pool_was-p_default_443=1020268972.47873.0000; JSESSIONID=0000aOA3oxjjUMisw1f8mnJuz9D:1cn7jtfnj; TS013694c2=0198c2d64430855160404b690b35fb7fe1bb31317d1763e8ea9dadeafcc75e5e4eb8e46e80da91e38394ceeb1750446fc8da49f95c'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
